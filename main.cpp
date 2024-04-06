f59bb152c225db5153a46cde3e9efcdf0ce89ee3
kiniikun <kaushal.panduru@gmail.com>
2024-04-06 09:53:44 +0000

    lab 1 code works.. i just need to add my comments!!
    Signed-off-by: kiniikun <kaushal.panduru@gmail.com>

--- main.cpp
+++ main.cpp
@@ -3,18 +3,22 @@
 
 InterruptIn buttn(PC_13);
 
+volatile int myflag = 0;
+
 void buttn_fall(){
-    printf("button pressed!!\r\n");
+    myflag = 1;
 }
 
 // main() runs in its own thread in the OS
 int main()
 {
     printf("This is mbed os v%d \r\n", MBED_VERSION);
-
     buttn.fall(&buttn_fall);
     while (true) {
-
+        if(myflag == 1){
+            printf("button pressed!!\r\n");
+            myflag = 0;
+        }
     }
 }
 
