ccb4261689e8db67025bcc7c3f00bf230f2787bd
kiniikun <kaushal.panduru@gmail.com>
2024-04-06 09:46:18 +0000

    lab 1 mbed OS error
    Signed-off-by: kiniikun <kaushal.panduru@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include "mbed.h"
+#include <cstdio>
 
+InterruptIn buttn(PC_13);
 
-//just putting in a comment to show a second commit and push
+void buttn_fall(){
+    printf("button pressed!!\r\n");
+}
 
 // main() runs in its own thread in the OS
 int main()
 {
+    printf("This is mbed os v%d \r\n", MBED_VERSION);
+
+    buttn.fall(&buttn_fall);
     while (true) {
+
     }
 }
 
